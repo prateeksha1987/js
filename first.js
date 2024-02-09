78db663ee6ca4be63007f939391f69d7f87683e8
prateeksha1987 <159522034+prateeksha1987@users.noreply.github.com>
2024-02-09 13:39:53 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,20 @@
+// let name="shivam";
+// document.getElementById("demo").innerHTML=name;
+
+
+// for(i=1;i<=10;i++)
+// {
+//     document.write(i  + "<br>");
+// }
+
+
+// for(i=10;i>=1;i--)
+// {
+//     document.write(i  + "<br>");
+// }
+
+
+for(i= 1;i<=10;i+1)
+{
+    document.write(i  + "<br>");
+}
---
+++ new.js
@@ -0,0 +1,13 @@
+document.write("Hello.. World" + "</br>");
+
+
+// let a = 0;
+// for (a = 1; a <= 10; a++) {
+//     document.write(a + "</br>");
+// }
+// for (a = 10; a >= 1; a--) {
+//     document.write(a + "</br>");
+// }
+for (a = 2; a <= 20;a= a+2) {
+    document.write(a + "</br>");
+}
